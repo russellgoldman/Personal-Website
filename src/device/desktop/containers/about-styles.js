6fb347ae2a55ecbbd92b45e0b6bfe442114eeb31
Russell Goldman <russellgoldman1@gmail.com>
2018-05-31 04:00:14 -0400

    Made about section fully responsive

--- src/device/desktop/containers/about-styles.js
+++ src/device/desktop/containers/about-styles.js
@@ -3,24 +3,24 @@ export const styles = {
     display: 'flex',
     flexDirection: 'column',
     flex: 1,
-    height: '85vh',
     marginLeft: '10em',
     marginRight: '10vw'
   },
   innerContainer: {
-    paddingTop: '4em',
+    marginTop: '4vw',
     display: 'flex',
     flex: 4,
     flexDirection: 'row',
-    height: '100%',
+    marginBottom: '1vw'
   },
   carouselContainer: {
     flex: 1
   },
   imageContainer: {
     display: 'flex',
+    flexDirection: 'column',
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     maxWidth: '25em'
   },
@@ -28,47 +28,94 @@ export const styles = {
     display: 'flex',
     flex: 3,
     flexDirection: 'column',
-    height: '100%',
-    marginLeft: '2em'
   },
   textContainer: {
     display: 'flex',
     flexDirection: 'column',
-    flex: 4,
-    fontFamily: 'Roboto',
     lineHeight: '2',
-    justifyContent: 'center',
-    height: '100%'
+    marginTop: '-1.5vw'
   },
-  buttonContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    flex: 1
-  },
-  bioImage: {
-    width: '350px',
-    height: '370px',
-    borderRadius: '30px',
-    marginRight: '30px'
-  },
-  button: {
-    fontSize: '26px',
+  textSmall: {
     fontFamily: 'Roboto',
-    letterSpacing: '3px',
     flex: 1,
+    fontSize: '14.5px',
+    marginLeft: '3.5vw'
+  },
+  textMedium: {
+    fontFamily: 'Roboto-Light',
+    flex: 1,
+    fontSize: '18px',
+    marginLeft: '5vw'
+  },
+  textLarge: {
+    fontFamily: 'Roboto-Light',
+    flex: 1,
+    fontSize: '22.5px',
+    marginLeft: '8vw'
+  },
+  bioImageSmall: {
+    width: '300px',
+    height: '300px',
+    borderRadius: '30px',
+    marginLeft: '-2.5vw'
+  },
+  bioImageMedium: {
+    width: '350px',
+    height: '350px',
+    borderRadius: '30px',
+    marginLeft: '2vw',
+  },
+  bioImageLarge: {
+    width: '400px',
+    height: '400px',
+    borderRadius: '30px',
+    marginLeft: '5vw',
+  },
+  buttonSmall: {
+    fontSize: '22px',
+    fontFamily: 'Roboto',
+    flex: 1,
+    letterSpacing: '3px',
     backgroundColor: '#00ABCD',
     color: 'white',
     borderRadius: '10px',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '50px',
-    width: '100%',
-    outline: 'none'
+    height: '40px',
+    maxWidth: '25%',
+    outline: 'none',
+    marginTop: '0.5em'
+  },
+  buttonMedium: {
+    fontSize: '22px',
+    fontFamily: 'Roboto',
+    flex: 1,
+    letterSpacing: '3px',
+    backgroundColor: '#00ABCD',
+    color: 'white',
+    borderRadius: '10px',
+    height: '45px',
+    maxWidth: '25%',
+    outline: 'none',
+    marginTop: '1em'
+  },
+  buttonLarge: {
+    fontSize: '30px',
+    fontFamily: 'Roboto',
+    flex: 1,
+    letterSpacing: '3px',
+    backgroundColor: '#00ABCD',
+    color: 'white',
+    borderRadius: '10px',
+    height: '60px',
+    maxWidth: '30%',
+    outline: 'none',
+    marginTop: '1em'
   },
   linkContainer: {
     display: 'flex',
     flexDirection: 'row',
-    flex: 1,
-    textDecoration: 'none'
+    width: '100%',
+    textDecoration: 'none',
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 };
--- src/device/desktop/containers/about.js
+++ src/device/desktop/containers/about.js
@@ -3,6 +3,7 @@ import DesktopIconCarousel from './../components/iconCarousel';
 import { ToolkitIcons } from './../../../data/toolkitIcons';
 import { styles } from './about-styles';
 import { bioImage } from './../../../images/';
+import Media from 'react-media';
 
 export class DesktopAbout extends Component {
   constructor(props) {
@@ -10,7 +11,91 @@ export class DesktopAbout extends Component {
     this.state = {
       icons: ToolkitIcons,
       maxRender: 6,
+      text: `Russell Goldman is a Computer Science undergrad going into his 3rd year at Laurier.
+      During his spare time, he volunteers with a local high school robotics team, associates with
+      an international fraternity, and improves upon his programming skills by participating
+      in hackathons and developing open-source software. He is currently dedicating a large amount
+      of his free time to mastering React (JS / Native) and NodeJS. He hopes to provide value at a software firm
+      during the summer of 2019.`
     }
+  }
+
+  renderResponsiveImage() {
+    return (
+      <div>
+        <Media
+          query={{ maxWidth: 1300 }}
+          render={() =>
+            <img src={bioImage} alt="Russell Goldman"
+            style={styles.bioImageSmall} />
+          }
+        />
+        <Media
+          query={{ minWidth: 1301, maxWidth: 1800 }}
+          render={() =>
+            <img src={bioImage} alt="Russell Goldman"
+            style={styles.bioImageMedium} />
+          }
+        />
+        <Media
+          query={{ minWidth: 1801 }}
+          render={() =>
+            <img src={bioImage} alt="Russell Goldman"
+            style={styles.bioImageLarge} />
+          }
+        />
+      </div>
+    );
+  }
+
+  renderResponsiveText() {
+    return (
+      <div style={styles.textContainer}>
+        <Media
+          query={{ maxWidth: 1300 }}
+          render={() =>
+            <p style={styles.textSmall}>{this.state.text}</p>
+          }
+        />
+        <Media
+          query={{ minWidth: 1301, maxWidth: 1800 }}
+          render={() =>
+            <p style={styles.textMedium}>{this.state.text}</p>
+          }
+        />
+        <Media
+          query={{ minWidth: 1801 }}
+          render={() =>
+            <p style={styles.textLarge}>{this.state.text}</p>
+          }
+        />
+      </div>
+    );
+  }
+
+  renderResponsiveButton() {
+    return (
+      <div style={styles.linkContainer}>
+        <Media
+          query={{ maxWidth: 1300 }}
+          render={() =>
+            <button style={styles.buttonSmall}>RESUME</button>
+          }
+        />
+        <Media
+          query={{ minWidth: 1301, maxWidth: 1800 }}
+          render={() =>
+            <button style={styles.buttonMedium}>RESUME</button>
+          }
+        />
+        <Media
+          query={{ minWidth: 1801 }}
+          render={() =>
+            <button style={styles.buttonLarge}>RESUME</button>
+          }
+        />
+      </div>
+    );
   }
 
   render() {
@@ -19,29 +104,18 @@ export class DesktopAbout extends Component {
         <div style={styles.outerContainer}>
           <div style={styles.innerContainer}>
             <div style={styles.imageContainer}>
-              <img src={bioImage} alt="Russell Goldman"
-                style={styles.bioImage} className="animated slideInLeft"/>
+              {this.renderResponsiveImage()}
             </div>
             <div style={styles.bioContainer}>
-              <div style={styles.textContainer} className="animated fadeIn">
-                <p style={{ fontSize: '16px' }}>Russell Goldman is a Computer Science undergrad going into his 3rd year at Laurier.
-                During his spare time, he volunteers with a local high school robotics team, associates with
-                an international fraternity, and improves upon his programming skills by participating
-                in hackathons and developing open-source software. He is currently dedicating a large amount
-                of his free time to mastering React (JS / Native) and NodeJS. He hopes to provide value at a software firm
-                during the summer of 2019.</p>
+              {this.renderResponsiveText()}
+              <div className="animated fadeIn">
+                <a href="https://drive.google.com/open?id=1pVsK5KqG6EgkJEE_ylNuQfWAGOZlGiHe" target="_blank"
+                  rel='noopener noreferrer' style={styles.linkContainer}>
+                  {this.renderResponsiveButton()}
+                </a>
               </div>
             </div>
           </div>
-          {/* move resume button to the contact section of the site */}
-          {/*<div style={styles.buttonContainer} className="animated fadeIn">
-          <div style={{ flex: 1 }}></div>
-            <a href="https://drive.google.com/open?id=1pVsK5KqG6EgkJEE_ylNuQfWAGOZlGiHe" target="_blank"
-              rel='noopener noreferrer' style={styles.linkContainer}>
-              <button style={styles.button}>RESUME</button>
-            </a>
-            <div style={{ flex: 1 }}></div>
-          </div>*/}
           <div style={{ paddingTop: '1em' }}></div>
           <div style={styles.carouselContainer}>
             <DesktopIconCarousel icons={this.state.icons}
